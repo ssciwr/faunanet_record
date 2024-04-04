123e440703b5591cea0a23059455cc7483e94bd9
Harald Mack <harald.mack@protonmail.com>
2024-04-04 09:58:37 +0200

    integrate cli into the main script

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from pathlib import Path
-from set_up_fake_sparrow import set_up
+from iSparrowRecord import set_up_sparrow as sus
 
-from iSparrowRecord import Runner
+from iSparrowRecord.runner import Runner
+from iSparrowRecord.cli import cli
 
 if __name__ == "__main__":
 
-    # hardcoded paths and args will be replaced with commandline arguments later,
-    # or optionally with config node
-    cfgdir = Path(__file__).resolve().parent / Path("config")
+    # # hardcoded paths and args will be replaced with commandline arguments later,
+    # # or optionally with config node
+    # cfgdir = Path(__file__).resolve().parent / Path("config")
 
-    runs_standalone = True
+    # runs_standalone = True
 
-    if runs_standalone:
-        print("setting up sparrow")
-        set_up()
+    # if runs_standalone:
+    #     print("setting up sparrow")
+    #     sus.set_up()
 
-    runner = Runner(cfgdir / "custom_example.yml")
+    # runner = Runner(cfgdir / "custom_example.yml")
 
-    runner.run()
+    # runner.run()
+    cli()
