5376caa7752b5f97e32dd2513b2a23579809e680
Harald Mack <harald.mack@protonmail.com>
2024-04-04 10:30:17 +0200

    remove old stuff from main that nobody needs

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-from pathlib import Path
-from iSparrowRecord import set_up_sparrow as sus
-
-from iSparrowRecord.runner import Runner
 from iSparrowRecord.cli import cli
 
 if __name__ == "__main__":
 
-    # # hardcoded paths and args will be replaced with commandline arguments later,
-    # # or optionally with config node
-    # cfgdir = Path(__file__).resolve().parent / Path("config")
-
-    # runs_standalone = True
-
-    # if runs_standalone:
-    #     print("setting up sparrow")
-    #     sus.set_up()
-
-    # runner = Runner(cfgdir / "custom_example.yml")
-
-    # runner.run()
     cli()
