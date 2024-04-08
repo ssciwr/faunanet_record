46374051213c880c4da36b740a4f5495f1f2c737
Harald Mack <harald.mack@protonmail.com>
2024-04-08 09:44:38 +0200

    correct cli, fix tests, add docstrings

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -9,7 +9,7 @@ def test_cli_install(folders, empty_data_folder):
 
     runner = CliRunner()
 
-    result_install = runner.invoke(cli.install, str(Path(custom_cfg)))
+    result_install = runner.invoke(cli.set_up, str(Path(custom_cfg)))
 
     assert result_install.exit_code == 0
 
@@ -23,7 +23,7 @@ def test_cli_install(folders, empty_data_folder):
         "",
     ]
 
-    result_install_failed = runner.invoke(cli.install, "nonexistant")
+    result_install_failed = runner.invoke(cli.set_up, "nonexistant")
 
     assert result_install_failed.exit_code == 1
 
