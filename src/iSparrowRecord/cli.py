80107e5b5a6672d95dc45f6aea4a5d555e353be3
Harald Mack <harald.mack@protonmail.com>
2024-04-16 08:49:51 +0200

    add cli code

---
+++ src/iSparrowRecord/cli.py
@@ -0,0 +1,101 @@
+from .utils import read_yaml, update_dict_recursive, dict_from_string
+from .runner import Runner
+from . import set_up_sparrow as sus
+
+from pathlib import Path
+import warnings
+import click
+from platformdirs import user_config_dir
+
+
+@click.group()
+def cli():
+    # empty because everything is done in subcommands
+    pass
+
+
+@cli.command()
+@click.argument("cfg", type=str)
+def set_up(cfg: str):
+    """
+    set_up Call the setup function of the project. Reads parameters (folder locations)
+    from a config file, creates them and copies the default configuration into the standard config folder of you system under 'iSparrowRecord'.
+
+    Args:
+        cfg (str): Set up configuration to use.
+    """
+    sus.set_up(cfg)
+
+
+@cli.command()
+@click.option("--cfg", help="custom configuration file", default="")
+@click.option("--debug", help="Enable debug output", is_flag=True)
+@click.option(
+    "--replace",
+    help="Replace any entry in the config with a different value. Mostly useful for debugging or testing purposes.",
+    default="{}",
+)
+def run(cfg: str, debug: bool, replace: dict):
+    """
+    run Create a runner object and record data using parameters read from a config file.
+
+    Args:
+        cfg (str): Config file to use.
+        debug (bool): _description_
+        replace (dict): Dictionary to replace elements of the given config with. Useful for debugging, trying things out, or for quick changes without having to create a new config file.
+
+    Raises:
+        FileNotFoundError: When there is not install config file in the user's config folder.
+        FileNotFoundError: When no data folder has been found.
+    """
+
+    # raise warning that no logging is there yet
+    print("Setting up data collection procedure")
+    if debug:
+        warnings.warn(
+            "Debug output currently not yet implemented. Will run, but without any debug output."
+        )
+
+    print("...preparing config")
+
+    install_path = Path(user_config_dir("iSparrowRecord")) / "install.yml"
+
+    if install_path.is_file() is False:
+        raise FileNotFoundError(
+            f"No install config file found at {install_path}. Has the system be set up by using the 'install' command line option first?"
+        )
+
+    # read install file and check if the system has been set up, raise if not
+    install_cfg = read_yaml(
+        str(Path(user_config_dir("iSparrowRecord")) / "install.yml")
+    )
+
+    data_folder = Path(install_cfg["Directories"]["data"]).expanduser()
+
+    if data_folder.is_dir() is False:
+        raise FileNotFoundError(
+            f"Folder {data_folder} not found. Has the system be set up by using the 'install' command line option first?"
+        )
+
+    replace_dict = dict_from_string(replace)
+
+    if cfg != "":
+        custom_filepath = Path(cfg).expanduser()
+
+        print("...using custom run config: ", custom_filepath)
+
+        custom_cfg = read_yaml(custom_filepath)
+
+        # when custom_cfg and replace have no top level nodes, they are merged, otherwise the leaves of custom_cfg is updated with replace.
+        if len(set(custom_cfg.keys()).intersection(set(replace_dict.keys()))) > 0:
+            update_dict_recursive(custom_cfg, replace_dict)
+        else:
+            custom_cfg = custom_cfg | replace_dict
+    else:
+        custom_cfg = replace_dict
+
+    print("...creating runner")
+    runner = Runner(custom_cfg)
+
+    print("...running data collection")
+    runner.run()
---
+++ tests/cli_test.py
@@ -0,0 +1,189 @@
+from click.testing import CliRunner
+from iSparrowRecord import cli
+from pathlib import Path
+import pytest
+
+
+def test_cli_install(folders, empty_data_folder):
+    _, _, custom_cfg = folders
+
+    runner = CliRunner()
+
+    result_install = runner.invoke(cli.set_up, str(Path(custom_cfg)))
+
+    assert result_install.exit_code == 0
+
+    output = result_install.output.split("\n")
+
+    assert output == [
+        "Creating iSparrow folders and downloading data",
+        f"...using install config {custom_cfg}",
+        "...making directories",
+        "Installation finished",
+        "",
+    ]
+
+    result_install_failed = runner.invoke(cli.set_up, "nonexistant")
+
+    assert result_install_failed.exit_code == 1
+
+    output = result_install_failed.output.split("\n")
+    assert output == [
+        "Creating iSparrow folders and downloading data",
+        "...using install config nonexistant",
+        "",
+    ]
+
+    assert type(result_install_failed.exception) is FileNotFoundError
+    assert result_install_failed.exception.args == (
+        "The folder nonexistant/install.yml does not exist",
+    )
+
+
+def test_cli_run_default(folders, empty_data_folder):
+    _, data, _ = folders
+
+    runner = CliRunner()
+
+    result = runner.invoke(cli.run)
+
+    # make sure things ran smoothly
+    assert result.exit_code == 0
+
+    data_dirs = list(Path(data).iterdir())
+    most_recent_folder = max(
+        data_dirs, key=lambda folder: folder.stat().st_mtime, default=None
+    )
+
+    yml_count = 0
+    wav_count = 0
+    for filename in Path(most_recent_folder).iterdir():
+
+        if filename.suffix == ".yml":
+            yml_count += 1
+
+        if filename.suffix == ".wav":
+            wav_count += 1
+
+    # check output
+    assert yml_count == 1
+    assert wav_count == 3
+
+
+def test_cli_run_debug(empty_data_folder):
+    runner = CliRunner()
+
+    with pytest.warns(UserWarning) as warning_info:
+        result = runner.invoke(cli.run, "--debug")
+
+    assert result.exit_code == 0
+
+    res = result.output.split("\n")
+    assert res == [
+        "Setting up data collection procedure",
+        "...preparing config",
+        "...creating runner",
+        "...running data collection",
+        "... ...collecting data for  9  seconds with  3 seconds per file",
+        "",
+    ]
+
+    assert (
+        str(warning_info[0].message)
+        == "Debug output currently not yet implemented. Will run, but without any debug output."
+    )
+
+
+def test_cli_run_custom(folders, empty_data_folder):
+    _, data, cfgdir = folders
+
+    runner = CliRunner()
+
+    path = str(Path(cfgdir) / "custom_example.yml")
+
+    data_dirs = list(Path(data).iterdir())
+
+    assert len(data_dirs) == 0
+
+    result = runner.invoke(cli.run, f"--cfg={path}")
+
+    assert result.exit_code == 0
+
+    res = result.output.split("\n")
+
+    assert res == [
+        "Setting up data collection procedure",
+        "...preparing config",
+        "...using custom run config:  " + path,
+        "...creating runner",
+        "...running data collection",
+        "... ...collecting data for  8  seconds with  4 seconds per file",
+        "",
+    ]
+
+    data_dirs = list(Path(data).iterdir())
+
+    assert len(data_dirs) == 1
+
+    most_recent_folder = max(
+        data_dirs, key=lambda folder: folder.stat().st_mtime, default=None
+    )
+
+    yml_count = 0
+
+    wav_count = 0
+
+    for filename in Path(most_recent_folder).iterdir():
+        if filename.suffix == ".yml":
+            yml_count += 1
+
+        if filename.suffix == ".wav":
+            wav_count += 1
+
+    # check output
+    assert yml_count == 1
+    assert wav_count == 2
+
+
+def test_cli_run_custom_replace(folders, empty_data_folder):
+    _, data, cfgdir = folders
+
+    runner = CliRunner()
+
+    path = str(Path(cfgdir) / "custom_example.yml")
+
+    dictstr = '\'{"Recording":{"length_s":2}}\''
+
+    data_dirs = list(Path(data).iterdir())
+
+    assert len(data_dirs) == 0
+
+    result = runner.invoke(cli.run, f"--cfg={path} --replace={dictstr}")
+
+    assert result.exit_code == 0
+
+    data_dirs = list(Path(data).iterdir())
+
+    most_recent_folder = max(
+        data_dirs, key=lambda folder: folder.stat().st_mtime, default=None
+    )
+
+    assert len(data_dirs) == 1
+
+    assert len(list(most_recent_folder.iterdir())) == 5
+
+    yml_count = 0
+
+    wav_count = 0
+
+    for filename in Path(most_recent_folder).iterdir():
+        if filename.suffix == ".yml":
+            yml_count += 1
+
+        if filename.suffix == ".wav":
+            wav_count += 1
+
+    # check output
+    assert yml_count == 1
+
+    assert wav_count == 4
