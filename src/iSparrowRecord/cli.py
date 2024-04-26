45969119fa56c12663c25fad03289db6442d0b62
Harald Mack <harald.mack@protonmail.com>
2024-04-26 10:55:40 +0200

    add help string to install

--- src/iSparrowRecord/cli.py
+++ src/iSparrowRecord/cli.py
@@ -15,9 +15,13 @@ def cli():
 
 
 @cli.command()
-@click.argument("cfg", type=str)
-def install(cfg: str):
-    sus.set_up(cfg)
+@click.argument(
+    "cfg_dir",
+    type=str,
+    help="The directory given must contain 'default.yml' and 'install.yml'. Check out the examples provided to see how to customize them.",
+)
+def install(cfg_dir: str):
+    sus.set_up(cfg_dir)
 
 
 @cli.command()
